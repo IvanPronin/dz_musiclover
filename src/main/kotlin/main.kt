511e0fbbbc93485f993b396732643c22d2b241ef
Ivan Pronin <vanyusha.pronin.@88gmail.com>
2021-09-30 21:35:06 +0400

    Modified task

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,22 @@
-fun main(){
-    var a: Int = 20_000
+fun main() {
 
-    if (a <= 1000){
-        println("Cкидка применяется только к сумме от 1001 рубля")
-    } else if(a == 1001 || a <= 10_000){
-        println("Скидка составляет 100 рублей")
-        var sum: Int = a - 100
-        println("Сумма со скидкой $sum рублей")
-    } else if(a == 10_001 || a > 10_001){
+    var sum: Int = 10_001
+
+    var isMusiclover: Boolean = (sum == 10_001 || sum > 10_001)
+
+    if(sum <= 1000){
+        println("Cкидка применяется только к сумме от 1001 рубля \nВаша сумма $sum рублей")
+    }else if(isMusiclover == true) {
         println("Скидка составляет 5% и т.к вы меломан еще вычитаем 1%")
-        var sum1: Int = (a * 5) / 100
-        var sum2: Int = a - sum1
-        var sum3: Int = (sum2 /100)
-        var result: Int = sum2 - sum3
-        println("Ваша сумма со скидками составляет $result рублей")
+        var discount: Int = sum - ((sum * 6) / 100)
+        println("Ваша сумма со скидкой составляет $discount рублей")
+    } else if (isMusiclover == false) {
+        var discount_1: Int = sum - 100
+        println("Ваша сумма со скидкой составляет $discount_1 рублей")
     }
+
+
+
+
 
 }
